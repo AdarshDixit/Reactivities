8efb4e93bd1ac25f02084d37e0460959f3f3f4eb
adarshdixit1995 <adarshdixit1995@gmail.com>
2020-05-07 20:51:26 +0530

    Reactivities initial setup

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Value> Get()
+        {
+            return new List<Value>();
+        }
+        [HttpGet("{id}")]
+        public Value Get(int id)
+        {
+            return new Value();
+        }
+    }
+}
